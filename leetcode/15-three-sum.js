447a409f1526d43edf756a5576f7d4d8489e97e5
samarth nagar <samarth07nagar@gmail.com>
2025-01-31 23:38:38 +0530

    main leetcode stuff 2

--- leetcode/15-three-sum.js
+++ leetcode/15-three-sum.js
@@ -1,37 +1,36 @@
-let len = Math.floor(Math.random() * (10 - +3)) + 3;
+let len = 10;
 
 let nums = [];
 
 for (let i = 0; i < len; i++) {
-	nums.push(Math.floor(Math.random() * (5 - +-5)) + -5);
+	nums.push(Math.floor(Math.random() * 11) - 5); // Random number between -5 and 5
 }
 
 // console.log(nums);
 
 let ans = [];
 
+ans.sort((a, b) => a[0] - b[0]); // Sort to avoid duplicate patterns
 for (let i = 0; i < nums.length; i++) {
-	for (let j = 0; j < nums.length; j++) {
-		for (let k = 0; k < nums.length; k++) {
+	for (let j = i + 1; j < nums.length; j++) {
+		for (let k = j + 1; k < nums.length; k++) {
 			if (nums[i] + nums[j] + nums[k] === 0) {
-				ans.push([nums[i], nums[j], nums[k]]);
+				ans.push([nums[i], nums[j], nums[k]].sort((a, b) => a - b)); // Sort to avoid duplicate patterns
 			}
 		}
 	}
 }
 
-ans.sort();
-// console.log(ans);
+// Remove duplicates using a Set
+let uniqueTriplets = [];
+let seen = new Set();
 
-for (let i = 0; i < ans.length; i++) {
-	ans[i].sort();
-}
-
-// console.log(ans);
-
-for (let i = 1; i < ans.length; i++) {
-	if (ans[i - 1] === ans[i]) {
-		ans.pop(i);
+for (let triplet of ans) {
+	let key = JSON.stringify(triplet);
+	if (!seen.has(key)) {
+		seen.add(key);
+		uniqueTriplets.push(triplet);
 	}
 }
-console.log(ans);
+
+console.log(uniqueTriplets);
