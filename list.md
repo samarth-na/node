f80add06469749b99e0519247afd4091bc98b77a
samarth nagar <samarth07nagar@gmail.com>
2025-09-14 12:24:43 +0530

    refacring removing

--- list.md
+++
@@ -1,9 +0,0 @@
-# methods
-parseInt
-find
-filter
-map
-forEach
-
-# data structure
-set
--- str.js
+++
@@ -1,8 +0,0 @@
-let string = (n) => {
-	let str = "";
-	for (let i = 0; i < n; i++) {
-		i % 2 != 0 ? (str += "K") : (str += "J");
-	}
-	console.log(str);
-};
-string(10);
