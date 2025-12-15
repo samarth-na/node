db87e6608a1a6a16ef54cf3f18f8b81dc0a64c35
samarth nagar <samarth07nagar@gmail.com>
2025-12-16 00:41:19 +0530

    backup

--- wiki/wikipedia.ts
+++ wiki/wikipedia.ts
@@ -1,12 +1,15 @@
+import Commander from "commander";
+const myapp = new Commander.Command();
+myapp.version("0.0.1");
 async function getData(params: string) {
-	const link = `https://en.wikipedia.org/w/api.php?action=opensearch&format=xml&search=${params}&namespace=0&limit=10&formatversion=2`;
-	const response = await fetch(link);
-	const data = await response.text(); // Correctly fetch the XML as text
-	console.log(data);
+    const link = `https://en.wikipedia.org/w/api.php?action=opensearch&format=xml&search=${params}&namespace=0&limit=10&formatversion=2`;
+    const response = await fetch(link);
+    const data = await response.text(); // Correctly fetch the XML as text
+    console.log(data);
 }
 
 async function main() {
-	getData("java");
+    getData("java");
 }
 
 main();
